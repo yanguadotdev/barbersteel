e23f6bd226c28f3bc01b6e28ab57ae084488e184
yanguadotdev <yanguasamir05@gmail.com>
2025-06-06 17:20:18 -0500

    feat: add slider component

--- src/data/site-data.ts
+++ src/data/site-data.ts
@@ -20,3 +20,49 @@ export const navLinks = [
     link: '#location'
   }
 ]
+
+
+export const heroSlider = [
+  {
+    imgsrc: '/hero/banner.webp',
+    type: 'video',
+    aspect: 'video',
+    box: {
+      label: '¿qué haremos por tí?',
+      labelLink: 'Ver video',
+      link: '',
+      positionX: 'right',
+      positionY: 'bottom'
+    }
+  },
+  {
+    imgsrc: '/hero/barber.webp',
+    type: 'image',
+    aspect: 'square',
+    box: {
+      label: 'conoce a nuestro barberos',
+      labelLink: 'Ver todos',
+      link: '#barbers',
+      positionX: 'left',
+      positionY: 'top'
+    }
+  },
+  {
+    imgsrc: '/hero/equipment.webp',
+    type: 'image',
+    aspect: 'square',
+    box: null
+  },
+  {
+    imgsrc: '/hero/luxury-atmosphere.webp',
+    type: 'image',
+    aspect: 'video',
+    box: {
+      label: 'ambiente de lujo',
+      labelLink: 'Ver fotos',
+      link: '#gallery',
+      positionX: 'right',
+      positionY: 'top'
+    }
+  }
+]
