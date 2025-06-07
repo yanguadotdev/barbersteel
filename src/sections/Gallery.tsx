9ec5d91860b3879a885840f515b57e30d633b321
yanguadotdev <yanguasamir05@gmail.com>
2025-06-07 15:55:14 -0500

    improve responsive in the gallery section

--- src/sections/Gallery.tsx
+++ src/sections/Gallery.tsx
@@ -3,6 +3,7 @@ import { useEffect, useRef, useState } from 'react'
 
 import { useTransform, useScroll, motion } from 'motion/react'
 import type { MotionValue } from 'motion'
+import { cn } from '@/lib/utils'
 
 const images = [
   '/gallery/photo01.webp',
@@ -32,9 +33,9 @@ export default function Gallery() {
 
   const { height } = dimension
 
-  const y = useTransform(scrollYProgress, [0, 1], [0, height * .9])
+  const y = useTransform(scrollYProgress, [0, 1], [0, height * 0.9])
   const y2 = useTransform(scrollYProgress, [0, 1], [0, height * 1.2])
-  const y3 = useTransform(scrollYProgress, [0, 1], [0, height * .65])
+  const y3 = useTransform(scrollYProgress, [0, 1], [0, height * 0.65])
   const y4 = useTransform(scrollYProgress, [0, 1], [0, height * 1])
 
   useEffect(() => {
@@ -64,9 +65,9 @@ export default function Gallery() {
       <div ref={gallery} className="bg-dark h-[175vh] overflow-hidden">
         <div className="relative -top-[50vh] flex h-[200vh] gap-[2vw] p-[2vw]">
           <Column y={y} images={[images[0], images[1], images[2]]} />
-          <Column y={y2} images={[images[3], images[4], images[5]]} />
+          <Column className="hidden sm:flex" y={y2} images={[images[3], images[4], images[5]]} />
           <Column y={y3} images={[images[6], images[7], images[8]]} />
-          <Column y={y4} images={[images[9], images[10], images[11]]} />
+          <Column className="2lg:flex hidden" y={y4} images={[images[9], images[10], images[11]]} />
         </div>
       </div>
       <div className="h-[25vh]"></div>
@@ -74,13 +75,23 @@ export default function Gallery() {
   )
 }
 
-const Column = ({ images, y }: { images: string[]; y: MotionValue }) => {
+interface ColumnProps extends React.HTMLAttributes<HTMLDivElement> {
+  className?: string
+  images: string[]
+  y: MotionValue
+}
+
+const Column = (props: ColumnProps) => {
+  const { className, images, y } = props
   return (
-    <motion.div className='relative w-1/4 h-full min-w-3xs flex flex-col gap-[2vw] whitespace-nowrap' style={{ y }}>
+    <motion.div
+      className={cn('relative flex h-full flex-1 flex-col gap-[2vw] whitespace-nowrap', className)}
+      style={{ y }}
+    >
       {images.map((src, i) => {
         return (
-          <div key={i} className='h-1/3 w-full relative overflow-hidden rounded-[.5vw]'>
-            <img className='size-full object-cover' src={`${src}`} alt="image" />
+          <div key={i} className="relative h-1/3 w-full overflow-hidden rounded-[.5vw]">
+            <img className="size-full object-cover" src={`${src}`} alt="image" />
           </div>
         )
       })}
