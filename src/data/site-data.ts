2777f4cb99c52c11e6b61dd1cdddb102d938fb97
yanguadotdev <yanguasamir05@gmail.com>
2025-06-07 09:42:37 -0500

    feat: add services section

--- src/data/site-data.ts
+++ src/data/site-data.ts
@@ -1,3 +1,7 @@
+import Beard from '/public/services/beard.svg'
+import FaceFacial from '/public/services/face-facial.svg'
+import Haircut from '/public/services/haircut.svg'
+
 export const navLinks = [
   {
     label: 'home',
@@ -58,5 +62,74 @@ export const heroSlider = [
       positionX: 'left',
       positionY: 'top'
     }
+  }
+]
+
+
+export const services = [
+  {
+    category: 'cortes de pelo',
+    icon: Haircut,
+    items: [
+      {
+        label: 'Corte clasico',
+        description: 'Un corte clásico que te dará un aspecto profesional y moderno.',
+        price: 'S/20'
+      },
+      {
+        label: 'corte de lujo',
+        description: 'Un corte que cambiara radicalmente tu estilo y te hara lucir increible',
+        price: 'S/40'
+      },
+      {
+        label: 'corte para niños',
+        description: 'Cortes de pelo para niños que te ayudaran a que tu niño se sienta cómodo',
+        price: 'S/15'
+      }
+    ]
+  },
+
+  {
+    category: 'beard',
+    icon: Beard,
+    items: [
+      {
+        label: 'recorte de barba',
+        description: 'Un recorte de barba que te ayudara a que tu barba se sienta cómoda',
+        price: 'S/20'
+      },
+      {
+        label: 'afeitado con toalla caliente',
+        description: 'Un afeitado con toalla caliente que te ayudara a que tu barba se sienta cómoda',
+        price: 'S/30'
+      },
+      {
+        label: 'recorte de bigote',
+        description: 'Un recorte de bigote que te ayudara a que tu bigote se sienta cómoda',
+        price: 'S/25'
+      }
+    ]
+  },
+
+  {
+    category: 'facial',
+    icon: FaceFacial,
+    items: [
+      {
+        label: 'limpieza facial',
+        description: 'Un limpieza facial que te ayudara a que tu piel se sienta cómoda',
+        price: 'S/15'
+      },
+      {
+        label: 'masaje facial',
+        description: 'Un masaje facial que te ayudara a que tu piel se sienta cómoda',
+        price: 'S/15'
+      },
+      {
+        label: 'lavado de cabello',
+        description: 'Un lavado de cabello que te ayudara a que tu cabello se sienta cómoda',
+        price: 'S/10'
+      }
+    ]
   }
 ]
